492a0e2defd97405e1c6b8e307cd69af21f97ce1
Vasyka <vvanast@yandex.ru>
2017-03-13 15:56:28 +0300

    Simple hash. Maybe works with old wrong algoritm
    Added simple hash(work on letters). Maybe works with old algorithm =>
    errors

--- WottonFederhenCountLibrary/MyClass.cs
+++ WottonFederhenCountLibrary/MyClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace WottonFederhenCountLibrary
 {
 	public static class WottonCount
@@ -42,7 +43,7 @@ namespace WottonFederhenCountLibrary
 			return i;
 		}
 		//CWF в первом окне
-		public static double CountInFirstFrame(char[] str, uint k, char[] nucl, double[] cwf, uint[] nucln)
+		public static double CountInFirstFrame(char[] str, int k, char[] nucl, double[] cwf, uint[] nucln, ref Dictionary<string, double> hash)
 		{
 			double sum = 0;
 			for (uint i = 0; i < k; i++)//считаем кол-во нуклеотидов в окне по типам
@@ -58,41 +59,53 @@ namespace WottonFederhenCountLibrary
 				nuclnSum += nucln[i];
 				for (int it = 1; it <= nucln[i + 1]; it++)
 				{
-					sum += Math.Log((nuclnSum + it) / it, 4);// (nucln[0]+it)/it * ... * (nucln[0]+nucln[1])/nucln[1]
-					//Console.Write(" {0}/{1} ", (nuclnSum + it), it);
-					//Console.WriteLine(" sum = " + sum);
+					sum += Math.Log((nuclnSum + it) / (double)it, 4);// (nucln[0]+it)/it * ... * (nucln[0]+nucln[1])/nucln[1]
+					Console.Write(" {0}/{1} ", (nuclnSum + it), it);
+					Console.WriteLine(" sum = " + sum);
 				}
 			}
 			cwf[0] = sum / k;
+			string s0 = new string(str);
+			hash.Add(s0.Substring(0, k), cwf[0]);
+			Console.WriteLine("For key = {0}, value = {1}.",s0.Substring(0, k), hash[s0.Substring(0, k)]);
 			return sum;
 		}
 		//Считает сложность по Вудон-Федерхену
-		public static void CountWF(char[] str, uint k, char[] nucl, out double[] cwf)
+		public static void CountWF(char[] str, int k, char[] nucl, out double[] cwf)
 		{
+			Dictionary<string, double> hash = new Dictionary<string, double>();
+
 			cwf = new double[str.Length - k + 1];
 			uint[] nucln = new uint[nucl.Length];//массив с количеством нуклеотидов в окне
-			double sum = CountInFirstFrame(str, k, nucl, cwf, nucln);//в первом окне
+			double sum = CountInFirstFrame(str, k, nucl, cwf, nucln, ref hash);//в первом окне
 			Console.WriteLine("Сдвигаем рамку:");
-			for (uint l = 1; l <= str.Length - k; l++)//двигаем рамку и добавляем новые члены в сумму и считаем cwf
+
+			string s = new string(str);
+
+			for (int l = 1; l <= str.Length - k; l++)//двигаем рамку и добавляем новые члены в сумму и считаем cwf
 			{
-				uint i, j;
-				double b;
-				i = CountLetter(nucl, str[l - 1]);//удаляемый символ
-				j = CountLetter(nucl, str[l - 1 + k]);//добавляемый символ
-				nucln[i]--;
-				//Console.Write("-" + nucl[i] + " ");
-				nucln[j]++;
-				//Console.Write("+" + nucl[j] + " ");
-				if (i != j)//если разные символы
-				{
-					if (nucln[i] == 0) b = 1.0 / nucln[j];
-					else b = (nucln[i] + 1) * 1.0 / nucln[j];
-					double a = Math.Log(b, 4);
-					//Console.Write("{0}/{1} {2} {3} ", (nucln[i] + 1), nucln[j], b, a);
-					sum = sum + Math.Log(b, 4);
+				if(!(hash.TryGetValue(s.Substring(l, k), out cwf[l]))){
+					uint i, j;
+					double b;
+					i = CountLetter(nucl, str[l - 1]);//удаляемый символ
+					j = CountLetter(nucl, str[l - 1 + k]);//добавляемый символ
+					nucln[i]--;
+					Console.Write("-" + nucl[i] + " ");
+					nucln[j]++;
+					Console.Write("+" + nucl[j] + " ");
+					if (i != j)//если разные символы
+					{
+						if (nucln[i] == 0) b = 1.0 / nucln[j];
+						else b = (nucln[i] + 1) * 1.0 / nucln[j];
+						double a = Math.Log(b, 4);
+						Console.Write("{0}/{1} {2} {3} ", (nucln[i] + 1), nucln[j], b, a);
+						sum = sum + Math.Log(b, 4);
+					}
+					Console.WriteLine("sum = " + sum);
+					cwf[l] = sum / k;
+					hash.Add(s.Substring(l, k), cwf[l]);
+					Console.WriteLine("For key = {0}, value = {1}.", s.Substring(l, k), hash[s.Substring(l, k)]);
 				}
-				//Console.WriteLine("sum = " + sum);
-				cwf[l] = sum / k;
 			}
 			Console.WriteLine();
 			for (int i = 0; i < cwf.Length; i++) Console.WriteLine("Сложность c {0} по {1} символ = {2}", i + 1, i + k, cwf[i]);//выводим посчитанные сложности
