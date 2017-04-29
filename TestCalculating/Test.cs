a20817b7e11a8a92eaa93e98e67e4fa14d3106d4
Vasyka <vvanast@yandex.ru>
2017-04-29 19:02:45 +0300

    A1. Smart hash. Works right. One example test was added.
    Algorithm 1 - simple logarithms summing with smart hash. Works right,
    but slow. One example test was added.

---
+++ TestCalculating/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+namespace TestCalculating
+{
+	using WottonFederhenCountLibrary;
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			char[] nucl = { 'A', 'T', 'G', 'C' };
+			Dictionary<string, double> hash = new Dictionary<string, double>();
+			Assert.AreEqual(WottonFederhenCountLibrary.WottonCount.CountInFirstFrame(new char[] { 'A', 'G', 'T', 'C', 'A', 'C' }, 3, nucl, new double[4], new uint[4], ref hash),Math.Log(6,4));//example test for CountInFirstFrame
+		}
+	}
+}
