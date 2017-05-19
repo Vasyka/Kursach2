1b98200886af62799aa277ce905c36b9c06f4f16
Vasyka <vvanast@yandex.ru>
2017-05-20 01:44:30 +0400

    Drawing graphics. Not linked with main program.
    Drawing random graphics - ZedGraph library. Urgent tasks:
    - To fix the repetition of graphics' colors after deleting/adding
    - To add choosing a file in file explorer
    - To add deleting a file from a list of files like file explorer\mouseclicking on one of legendas
    - To link graphics with main program)

---
+++ WindowsFormsKurs/WindowsFormsKurs/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace WindowsFormsKurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addButton = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.rmvButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(246, 426);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(109, 42);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Добавить график";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(675, 417);
+            this.zedGraphControl1.TabIndex = 1;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // rmvButton
+            // 
+            this.rmvButton.Location = new System.Drawing.Point(387, 427);
+            this.rmvButton.Name = "rmvButton";
+            this.rmvButton.Size = new System.Drawing.Size(104, 41);
+            this.rmvButton.TabIndex = 2;
+            this.rmvButton.Text = "Удалить график";
+            this.rmvButton.UseVisualStyleBackColor = true;
+            this.rmvButton.Click += new System.EventHandler(this.rmvButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(684, 481);
+            this.Controls.Add(this.rmvButton);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Курсовая";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button rmvButton;
+    }
+}
+
